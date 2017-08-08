4dbbf307e88b1331d584d1228c39fdd6f682f809
Parth Patel <parth.orbit@gmail.com>
2017-08-08 23:25:53 +0530

    Remove document.getElementById code to set value in component, use State to set values.

--- EmployeeRecordBook.jsx
+++ EmployeeRecordBook.jsx
@@ -6,8 +6,14 @@ class EmployeeRecordBook extends React.Component {
         this.state = {
             employeeRecords: [],
             selectedIndex: 0,
-            updateButtonVisible : false,
-            cancelButtonVisible : false
+            updateButtonVisible: false,
+            cancelButtonVisible: false,
+            selectedEmp: {
+                empName: "",
+                empDept: "HR",
+                empDesg: "",
+                empHobby: ""
+            }
         };
         this.addEmployeeRecord = this.addEmployeeRecord.bind(this);
         this.editEmployeeRecord = this.editEmployeeRecord.bind(this);
@@ -18,60 +24,72 @@ class EmployeeRecordBook extends React.Component {
     addEmployeeRecord(formData) {
         let l_empRecords = this.state.employeeRecords;
         l_empRecords.push(formData);
-        this.setState({ employeeRecords: l_empRecords });
+        //this.setState({ employeeRecords: l_empRecords });
+        let l_selectedEmp = this.state.selectedEmp;
 
-        document.getElementById("empName").value = "";
-        document.getElementById("empDept").value = "HR";
-        document.getElementById("empDesg").value = "";
-        document.getElementById("empHobby").value = "";
-        //setEmpDataToForm(null);
+        this.setState({
+            employeeRecords: l_empRecords,
+            selectedEmp: l_selectedEmp
+        });
     }
     editEmployeeRecord(formData) {
         const l_empRecords = Array.from(this.state.employeeRecords);
         l_empRecords[this.state.selectedIndex] = formData;
-        this.setState({ employeeRecords : l_empRecords });
-        document.getElementById("empName").value = "";
-        document.getElementById("empDept").value = "HR";
-        document.getElementById("empDesg").value = "";
-        document.getElementById("empHobby").value = "";
-        //setEmpDataToForm(null);
+
+        let l_selectedEmp = this.state.selectedEmp;
+
+        l_selectedEmp.empName = "";
+        l_selectedEmp.empDept = "HR";
+        l_selectedEmp.empDesg = "";
+        l_selectedEmp.empHobby = "";
+
+        this.setState({ selectedEmp: l_selectedEmp, updateButtonVisible: false, cancelButtonVisible: false, employeeRecords: l_empRecords });
+
     }
     deleteEmployeeRecord(index) {
         const l_empRecords = Array.from(this.state.employeeRecords);
-        l_empRecords.splice(index,1);
-        this.setState({ employeeRecords : l_empRecords });
-        document.getElementById("empName").value = "";
-        document.getElementById("empDept").value = "HR";
-        document.getElementById("empDesg").value = "";
-        document.getElementById("empHobby").value = "";
-        //setEmpDataToForm(null);
+        l_empRecords.splice(index, 1);
+        let l_selectedEmp = this.state.selectedEmp;
+
+        l_selectedEmp.empName = "";
+        l_selectedEmp.empDept = "HR";
+        l_selectedEmp.empDesg = "";
+        l_selectedEmp.empHobby = "";
+
+        this.setState({ selectedEmp: l_selectedEmp, employeeRecords: l_empRecords });
     }
     setEmployeeDataToEdit(index) {
-        // setEmpDataToForm(this.state.employeeRecords[index]);
-        let l_empData=this.state.employeeRecords[index];
-        document.getElementById("empName").value = l_empData.empName;
-        document.getElementById("empDept").value = l_empData.empDept;
-        document.getElementById("empDesg").value = l_empData.empDesg;
-        document.getElementById("empHobby").value = l_empData.empHobby;
-        this.setState({updateButtonVisible : true,cancelButtonVisible:true,
-        selectedIndex:index});
+
+        let l_empData = this.state.employeeRecords[index];
+        let l_selectedEmp = this.state.selectedEmp;
+
+        l_selectedEmp.empName = l_empData.empName;
+        l_selectedEmp.empDept = l_empData.empDept;
+        l_selectedEmp.empDesg = l_empData.empDesg;
+        l_selectedEmp.empHobby = l_empData.empHobby;
+        this.setState({
+            updateButtonVisible: true, cancelButtonVisible: true,
+            selectedIndex: index, selectedEmp: l_selectedEmp
+        });
     }
-    cancelEdit()
-    {
-        document.getElementById("empName").value = "";
-        document.getElementById("empDept").value = "HR";
-        document.getElementById("empDesg").value = "";
-        document.getElementById("empHobby").value = "";
-        this.setState({updateButtonVisible : false,cancelButtonVisible:false,
-        selectedIndex:0});
+    cancelEdit() {
+        let l_selectedEmp = this.state.selectedEmp;
+
+        l_selectedEmp.empName = "";
+        l_selectedEmp.empDept = "HR";
+        l_selectedEmp.empDesg = "";
+        l_selectedEmp.empHobby = "";
+        this.setState({
+            updateButtonVisible: false, cancelButtonVisible: false,
+            selectedIndex: 0, selectedEmp: l_selectedEmp
+        });
     }
     render() {
         return (
             <div>
                 <EmployeeRecord addEmpployee={this.addEmployeeRecord} editEmployee={this.editEmployeeRecord}
-                cancelEdit={this.cancelEdit}
-                updateButton = {this.state.updateButtonVisible}
-                        cancelButton = {this.state.cancelButtonVisible} />
+                    cancelEdit={this.cancelEdit} updateButton={this.state.updateButtonVisible}
+                    selectedEmp={this.state.selectedEmp} cancelButton={this.state.cancelButtonVisible} />
                 <div className={styles.tableMain}>
                     <div className={styles.titleBar}>
                         <p>Employee Record Book</p>
@@ -84,7 +102,7 @@ class EmployeeRecordBook extends React.Component {
                     </div>
                     <EmployeeData localEmployeeRecords={this.state.employeeRecords}
                         localSetEmployeeDataToEdit={this.setEmployeeDataToEdit} localDeleteEmployeeData={this.deleteEmployeeRecord}
-                        />
+                    />
                 </div>
             </div>
         );
@@ -93,52 +111,61 @@ class EmployeeRecordBook extends React.Component {
 
 class EmployeeRecord extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            empName: "",
-            empDept: "",
-            empDesg: "",
-            empHobby: ""
+            empName: this.props.selectedEmp.empName,
+            empDept: this.props.selectedEmp.empDept,
+            empDesg: this.props.selectedEmp.empDesg,
+            empHobby: this.props.selectedEmp.empHobby
         };
         this.localAddEmployeeRecord = this.localAddEmployeeRecord.bind(this);
         this.localEditEmployeeRecord = this.localEditEmployeeRecord.bind(this);
         this.localCancelEdit = this.localCancelEdit.bind(this);
         this.updateState = this.updateState.bind(this);
     }
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            empName: this.props.selectedEmp.empName,
+            empDept: this.props.selectedEmp.empDept,
+            empDesg: this.props.selectedEmp.empDesg,
+            empHobby: this.props.selectedEmp.empHobby
+        });
+
+    }
     localAddEmployeeRecord(e) {
         this.props.addEmpployee(this.state);
     }
     localEditEmployeeRecord(e) {
-        console.log(this.state);
         this.props.editEmployee(this.state);
     }
-    localCancelEdit(e)
-    {    
+    localCancelEdit(e) {
         this.props.cancelEdit();
     }
     updateState(e) {
         this.setState({ [e.target.id]: e.target.value });
     }
-    render() {        
+    render() {
         return (
             <div>
-                Name : <input type="text" placeholder="Employee Name" id="empName" onChange={this.updateState} /><br />
-                Department : <select id="empDept" onChange={this.updateState}> <option value="HR">HR</option>
+                Name : <input type="text" placeholder="Employee Name" id="empName" onChange={this.updateState}
+                    value={this.state.empName} /><br />
+                Department : <select id="empDept" onChange={this.updateState} value={this.state.empDept}> <option value="HR">HR</option>
                     <option value="PRD">PRD</option>
                     <option value="IMPL">IMPL</option>
                     <option value="UI">UI</option>
                     <option value="MI">MI</option>
                     <option value="QA">QA</option>
                 </select><br />
-                Designation: <input type="text" placeholder="Employee Designation" id="empDesg" onChange={this.updateState} /><br />
-                Hobby : <textarea id="empHobby" rows="5" cols="25" placeholder="Hobby" onChange={this.updateState}></textarea><br />
-                {this.props.updateButton?
-                <button name="Update Employee" onClick={this.localEditEmployeeRecord}>Update Employee</button>:
-                <button name="Add Employee" onClick={this.localAddEmployeeRecord}>Add Employee</button>}
+                Designation: <input type="text" placeholder="Employee Designation" id="empDesg" onChange={this.updateState} value={this.state.empDesg} /><br />
+                Hobby : <textarea id="empHobby" rows="5" cols="25" placeholder="Hobby" onChange={this.updateState}
+                    value={this.state.empHobby}></textarea><br />
+                {this.props.updateButton ?
+                    <button name="Update Employee" onClick={this.localEditEmployeeRecord}>Update Employee</button> :
+                    <button name="Add Employee" onClick={this.localAddEmployeeRecord}>Add Employee</button>}
                 {this.props.cancelButton ?
-                <button name="Cancel" onClick={this.localCancelEdit}>Cancel</button>:
-                ""}
-                
+                    <button name="Cancel" onClick={this.localCancelEdit}>Cancel</button> :
+                    ""}
+
             </div>
         );
     }
@@ -153,7 +180,7 @@ class EmployeeData extends React.Component {
     localSetEmployeeDataToEdit(e) {
         this.props.localSetEmployeeDataToEdit(e.target.name);
     }
-    localDeleteEmployeeData(e){
+    localDeleteEmployeeData(e) {
         this.props.localDeleteEmployeeData(e.target.name);
     }
 
@@ -167,8 +194,7 @@ class EmployeeData extends React.Component {
                         <div className={styles.Cell}>{emp.empName}</div>
                         <div className={styles.Cell}>{emp.empDept}</div>
                         <div className={styles.Cell}>{emp.empDesg}</div>
-                        <div className={styles.Cell}>{emp.empHobby}</div>
-                        {/* <div><input type="hidden" id={emp.empName+i}/></div> */}
+                        <div className={styles.Cell}>{emp.empHobby}</div>                        
                         <div><button onClick={this.localSetEmployeeDataToEdit} name={i}>Edit</button></div>
                         <div><button onClick={this.localDeleteEmployeeData} name={i}>Delete</button></div>
                     </div>);
